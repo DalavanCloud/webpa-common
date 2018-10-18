482a1be4823189a431ce7abc9e7cc50dab4e7263
johnabass <johnbass1017@yahoo.com>
2018-10-18 15:45:41 -0700

    New package hierarchy to temporarily hold the new work

---
+++ device2/doc.go
@@ -0,0 +1,6 @@
+/*
+Package device2 is intended to replace the device package eventually.
+Certain functionality in the device package, such as the id parsing, will
+remain as is.
+*/
+package device2
